3681c3880db804beef01036438a65d1a5bfab569
vale-hash <newlechim@gmail.com>
2024-05-01 21:30:52 +0100

    The comments have been added

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,8 @@
 --create the database if hbnb_dev_db if not EXISTS
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- creates a user if it does not exist and gives a password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+--graants permissions to the user on hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+--grants onlt select permistions to the useron performance schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
