afe746093f790cf78cefac09e58ff67dac4b358a
vale-hash <newlechim@gmail.com>
2024-05-20 12:22:26 +0100

    sql database test

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,5 @@
-
---create the database if hbnb_dev_db if not EXISTS
+-- cerate sql db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- creates a user if it does not exist and gives a password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---graants permissions to the user on hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
---grants onlt select permistions to the useron performance schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
---flush priviledges for changes to take effect
-FLUSH PRIVILEGES;
